7b708e1de0cd44103b198ab5c2730cf88ebce070
Cody Kimb <codykimbell@gmail.com>
2020-12-30 14:04:07 -0600

    created array with question objects

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// array with questionObjects
+
+questionObjects = [
+    {
+        question: "Commonly used data types do NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        uestion: "The condition in and if/else statement is enclosed with ___.",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "parenthesis"
+    },
+    {
+        uestion: "Arrays in Javascript can be used to store ___.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        uestion: "String valyes must be enclosed within ___ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "commas"
+    },
+    {
+        uestion: "A very useful tool used during development and debugging for porinting content to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+]
